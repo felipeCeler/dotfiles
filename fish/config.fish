9536a546c4fbc751ba8280400718597c431d644e
felipe lenovo x1 <felipe.celer@gmail.com>
2019-12-21 03:39:06 -0300

    Start using fish abbr

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,15 @@
-# theme-bobthefish
+# prompt decoration
+set -g theme_display_user yes
+set -g theme_title_use_abbreviated_path no
+set -g fish_prompt_pwd_dir_length 0
+
+# set of abbriviations
+# portage
+abbr ew 'emerge -auDN @world --exclude="debian-sources"'
+abbr ed 'emerge --depclean'
+# git
+abbr gm 'git push origin master'
+abbr gp 'git pull origin master'
+abbr gs 'git status'
+abbr gc 'git commit'
+abbr ga 'git add'
