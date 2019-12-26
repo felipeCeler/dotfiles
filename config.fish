e9eee810fe066b1f8151de7d574200760a60e93c
felipe lenovo x1 <felipe.celer@gmail.com>
2019-12-25 22:51:48 -0300

    Added my custom config for my favourite theme: bobthefish

--- config.fish
+++
@@ -1,3 +0,0 @@
-set -g theme_display_date no
-set -g theme_nerd_fonts yes
-set -g theme_display_k8s_context no
--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,9 @@
 set -g theme_display_user yes
 set -g theme_title_use_abbreviated_path no
 set -g fish_prompt_pwd_dir_length 0
+set -g theme_powerline_fonts no
+set -g theme_nerd_fonts yes
+
 
 # set of abbriviations
 # portage
@@ -13,3 +16,5 @@ abbr gp 'git pull origin master'
 abbr gs 'git status'
 abbr gc 'git commit'
 abbr ga 'git add'
+# Alias
+alias rm='echo "This is not the command you are looking for."; false'
