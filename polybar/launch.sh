6553fe51f405e5975b76d3cd86e8a2da7a18807a
Felipe de Carvalho <felipe.celer@gmail.com>
2021-12-13 19:05:20 -0300

    Fixed forest theme issues

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -22,8 +22,8 @@ launch_bar() {
 	elif [[ "$style" == "pwidgets" ]]; then
 		bash "$dir"/pwidgets/launch.sh --main
 	else
-		MONITOR=HDMI-0 polybar -q main -c "$dir/$style/config_i3.ini" &
-		MONITOR=DP-2 polybar -q secondary -c "$dir/$style/config_i3.ini" &		
+		MONITOR=HDMI-0 polybar -q primary -c "$dir/$style/config.ini" &
+		MONITOR=DP-2 polybar -q secondary -c "$dir/$style/config.ini" &		
 	fi
 }
 
