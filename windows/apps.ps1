77a48a1fe00f090ca8a47daf83bcc7aa5f82e204
felipemc <felipemc@tecgraf.puc-rio.br>
2022-10-25 14:21:20 -0300

    added app.ps1

---
+++ windows/apps.ps1
@@ -0,0 +1,19 @@
+# Development
+winget install -e --id Notepad++.Notepad++
+winget install -e --id WinMerge.WinMerge
+winget install -e --id Git.Git
+winget install -e --id Kitware.CMake
+winget install -e --id Microsoft.VisualStudioCode
+winget install -e --id Docker.DockerDesktop
+winget install -e --id SoftwareFreedomConservancy.QEMU
+
+# Produtivity
+winget install JanDeDobbeleer.OhMyPosh -s winget
+
+# Media
+winget install -e --id CNRISTI.MeshLab
+winget install -e --id VideoLAN.VLC
+winget install -e --id BlenderFoundation.Blender
+winget install -e --id Inkscape.Inkscape
+winget install -e --id GIMP.GIMP
+winget install -e --id XnSoft.XnViewMP
